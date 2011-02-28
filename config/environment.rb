24ef94085adbc47e446eb34926d80c8d7cab7179
mwatha <mwatha.bwanalie@baobabhealth.org>
2011-02-28 10:54:38 +0200

    working on the migration script - adding Bart10.2 models 'needed only for migration'

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,18 @@ require 'bantu_soundex'
 require 'json'
 require 'colorfy_strings'
 
+migration = YAML.load(File.open(File.join(RAILS_ROOT, "config/database.yml"), "r"))['migration']
+PatAddress.establish_connection(migration)
+PatName.establish_connection(migration)
+PatIdentifier.establish_connection(migration)
+Pat.establish_connection(migration)
+Orders.establish_connection(migration)
+DrugOrders.establish_connection(migration)
+Enc.establish_connection(migration)
+Obs.establish_connection(migration)
+Relationships.establish_connection(migration)
+Users.establish_connection(migration)
+
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'person_address', 'person_address'
   inflect.irregular 'obs', 'obs'
