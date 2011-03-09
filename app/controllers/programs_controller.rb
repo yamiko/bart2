7f9ff03053e0b5a2e85a0e80471d732d05cccd04
mwatha <mwatha.bwanalie@baobabhealth.org>
2011-03-09 14:34:27 +0200

    issue #185

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -126,6 +126,10 @@ class ProgramsController < ApplicationController
       end
     else
       patient_program = PatientProgram.find(params[:id])
+      unless patient_program.date_completed.blank?
+        redirect_to :controller => :patients, :action => :programs, 
+          :patient_id => patient_program.patient.id, :error => "Patient have completed this program" and return
+      end
       @patient = patient_program.patient
       @patient_program_id = patient_program.patient_program_id
       program_workflow = ProgramWorkflow.all(:conditions => ['program_id = ?', patient_program.program_id], :include => :concept)
