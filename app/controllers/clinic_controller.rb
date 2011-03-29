1a37adbe4b45a354e2c523a416cdb81bc6bbfa9a
mwatha <mwatha.bwanalie@baobabhealth.org>
2011-03-29 15:56:01 +0200

    created a UI for entering clinic holidays,site code and clinic week days

--- app/controllers/clinic_controller.rb
+++ app/controllers/clinic_controller.rb
@@ -10,14 +10,19 @@ class ClinicController < ApplicationController
   end
 
   def reports
-    @reports = [["Cohort","/cohort_tool/cohort_menu"],["Supervision","/clinic/supervision"], ["Data Cleaning Tools", "/report/data_cleaning"], ["Stock report","/drug/date_select"]]
+    @reports = [
+                ["Cohort","/cohort_tool/cohort_menu"],
+                ["Supervision","/clinic/supervision"], 
+                ["Data Cleaning Tools", "/report/data_cleaning"], 
+                ["Stock report","/drug/date_select"]
+              ]
     render :template => 'clinic/reports', :layout => 'clinic' 
   end
 
   def supervision
-    @supervision_tools = [["Data that was Updated", "summary_of_records_that_were_updated"],
-                          ["Drug Adherence Level",    "adherence_histogram_for_all_patients_in_the_quarter"],
-                          ["Visits by Day",           "visits_by_day"],
+    @supervision_tools = [["Data that was Updated","summary_of_records_that_were_updated"],
+                          ["Drug Adherence Level","adherence_histogram_for_all_patients_in_the_quarter"],
+                          ["Visits by Day", "visits_by_day"],
                           ["Non-eligible Patients in Cohort", "non_eligible_patients_in_cohort"]]
 
    @landing_dashboard = 'clinic_supervision'
@@ -26,7 +31,13 @@ class ClinicController < ApplicationController
   end
 
   def properties
-    @settings = [["Set clinic days","#"],["Set clinic holidays","/properties/set_clinic_holidays"], ["Set site code", "#"]]
+    @settings = [
+                  ["View clinic days","/properties/show_clinic_days"],
+                  ["Set clinic days","/properties/clinic_days"],
+                  ["View clinic holidays","/properties/clinic_holidays"],
+                  ["Set clinic holidays","/properties/set_clinic_holidays"], 
+                  ["Set site code", "/properties/site_code"]
+                ]
     render :template => 'clinic/properties', :layout => 'clinic' 
   end
 
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,15 +4,73 @@ class PropertiesController < ApplicationController
   end
 
   def create_clinic_holidays
-    clinic_holidays = GlobalProperty.find_by_property('clinic.holidays')
-    if clinic_holidays.blank?
-      clinic_holidays = GlobalProperty.new()  if clinic_holidays.blank?
-      clinic_holidays.property = 'clinic.holidays'
-      clinic_holidays.description = 'day month year when clinic will be closed'
+    if request.post?
+      clinic_holidays = GlobalProperty.find_by_property('clinic.holidays')
+      if clinic_holidays.blank?
+        clinic_holidays = GlobalProperty.new()  
+        clinic_holidays.property = 'clinic.holidays'
+        clinic_holidays.description = 'day month year when clinic will be closed'
+      end
+      clinic_holidays.property_value = params[:holidays]
+      clinic_holidays.save 
+      flash[:notice] = 'Date(s) successfully created.'
     end
-    clinic_holidays.property_value = params[:holidays]
-    clinic_holidays.save
-    redirect_to '/clinic/properties' and return
+    redirect_to '/properties/clinic_holidays' and return
   end
 
+  def clinic_holidays
+    @holidays = GlobalProperty.find_by_property('clinic.holidays').property_value rescue []
+    @holidays = @holidays.split(',').collect{|date|date.to_date}.sort unless @holidays.blank?
+    render :layout => "menu"
+  end
+
+  def clinic_days
+    if request.post?
+      if params[:age_group] == 'Children'
+        clinic_days = GlobalProperty.find_by_property('peads.clinic.days')
+      else
+        clinic_days = GlobalProperty.find_by_property('clinic.days')
+      end
+
+      if clinic_days.blank?
+        clinic_days = GlobalProperty.new()  
+        clinic_days.property = 'clinic.days'
+        clinic_days.property = 'peads.clinic.days' if params[:age_group] == 'Children'
+        clinic_days.description = 'Week days when the clinic is open'
+      end
+      weekdays = params[:weekdays].split(',').collect{ |wd|wd.capitalize }
+      clinic_days.property_value = weekdays.join(',') 
+      clinic_days.save 
+      flash[:notice] = "Week day(s) successfully created.  (#{params[:age_group]})"
+      redirect_to "/properties/show_clinic_days" and return
+    end
+    render :layout => "menu"
+  end
+
+  def show_clinic_days
+    @clinic_days = week_days('clinic.days')
+    @peads_clinic_days = week_days('peads.clinic.days')
+    render :layout => "menu"
+  end
+
+  def week_days(property)
+    wkdays = {}
+    days = GlobalProperty.find_by_property(property).property_value rescue ''
+    days.split(',').map do | day |
+      wkdays[day] = 'X'
+    end rescue nil
+    return wkdays
+  end
+
+  def site_code
+    if request.post?
+      location = Location.find(Location.current_health_center.id)
+      location.neighborhood_cell = params[:site_code]
+      if location.save
+        flash[:notice] = "Site code successfully created.  (#{params[:site_code]})"
+      else
+        flash[:error] = "Site code not created.  (#{params[:site_code]})"
+      end
+    end
+  end
 end
