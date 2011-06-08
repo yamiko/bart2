62e998295095b860a3345ad00e31bdf2a77ee993
Yamiko <yamikomsosa@yahoo.co.uk>
2011-06-08 15:43:47 +0200

    Resolved issue #1404 - Barring enrolment of multiple programs over the same period

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -8,7 +8,7 @@ class ProgramsController < ApplicationController
                                     :joins => "INNER JOIN location l ON l.location_id = patient_program.location_id
                                                INNER JOIN program p ON p.program_id = patient_program.program_id",
                                     :select => "p.name program_name ,l.name location_name,patient_program.date_completed date_completed",
-                                    :conditions =>["voided = 0 AND patient_id = ?",params[:patient_id]]
+                                    :conditions =>["voided = 0 AND patient_id = ? AND date_completed IS NULL",params[:patient_id]]
                                     ).map{|pat_program|
                                       [pat_program.program_name,pat_program.location_name] if pat_program.date_completed.blank?
                                     }
@@ -22,8 +22,10 @@ class ProgramsController < ApplicationController
     invalid_date = false
     initial_date = params[:initial_date].to_date
     active_programs.map{ | program |
-      next if program.date_completed.blank? and program.date_enrolled.blank?
-      invalid_date = (initial_date >= program.date_enrolled.to_date and initial_date < program.date_completed.to_date)
+		if !(program.date_completed.blank? and program.date_enrolled.blank?)
+			#raise "Initial date -> " + initial_date.to_s + " Date enrolled -> " + program.date_enrolled.to_date.to_s + " Date completed -> " + program.date_completed.to_date.to_s
+      		invalid_date = (initial_date >= program.date_enrolled.to_date and initial_date < program.date_completed.to_date)
+		end
     }
 
     if invalid_date
@@ -134,13 +136,15 @@ class ProgramsController < ApplicationController
           #unless patient_program.update_attributes({:date_completed => Time.now()})
            # flash[:notice] = "OOps! Program completed date was not updated!."
           #end
-          date_completed = session[:datetime].to_time rescue Time.now()
+
+          # date_completed = session[:datetime].to_time rescue Time.now()
+          date_completed = params[:current_date].to_date rescue Time.now()
           PatientProgram.update_all "date_completed = '#{date_completed.strftime('%Y-%m-%d %H:%M:%S')}'",
                                      "patient_program_id = #{patient_program.patient_program_id}"
         end
         redirect_to :controller => :patients, :action => :programs, :patient_id => params[:patient_id]
       else
-        redirect_to :controller => :patients, :action => :programs, :patient_id => params[:patient_id]
+        redirect_to :controller => :patients, :action => :programs, :patient_id => params[:patient_id], :error => "Unable to update state"
       end
     else
       patient_program = PatientProgram.find(params[:id])
