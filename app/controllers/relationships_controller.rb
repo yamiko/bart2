866d036304a5c08aa2dc9ac68a5a893b03cc781c
gamphani <dave.phiri@baobabhealth.org>
2011-09-16 19:01:25 +0200

    Handle creation of TB Index person relation

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -15,15 +15,30 @@ class RelationshipsController < ApplicationController
   end
   
   def create
-    @relationship = Relationship.new(
-      :person_a => @patient.patient_id,
-      :person_b => params[:relation],
-      :relationship => params[:relationship])
-    if @relationship.save
-      redirect_to session[:return_to] and return unless session[:return_to].blank?
-      redirect_to :controller => :patients, :action => :guardians_dashboard, :patient_id => @patient.patient_id
-    else 
-      render :action => "new" 
+    relationship_id = params[:relationship].to_i rescue nil 
+    if relationship_id == RelationshipType.find_by_b_is_to_a('Other').id
+      person_id = params[:person].to_i
+      if person_id == 0 #if the person does not exist in db
+        person = Person.create_from_form({'names' => 
+                                           {'family_name' => params[:family_name],
+                                            'given_name' => params[:given_name]
+                                         },'gender' => params[:gender]
+                                        } 
+                                        )
+        person_id = person.id
+      end
+
+    else
+      @relationship = Relationship.new(
+        :person_a => @patient.patient_id,
+        :person_b => params[:relation],
+        :relationship => params[:relationship])
+      if @relationship.save
+        redirect_to session[:return_to] and return unless session[:return_to].blank?
+        redirect_to :controller => :patients, :action => :guardians_dashboard, :patient_id => @patient.patient_id
+      else 
+        render :action => "new" 
+      end
     end
   end
   
