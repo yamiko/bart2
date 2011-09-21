395eeec01b0385aaf5e07836ce380654b1b4aa06
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2011-09-21 23:27:30 +0200

    modified 'location' search for: 'Tranfer out list' and 'Local TB DOT site'

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -63,7 +63,14 @@ class ProgramsController < ApplicationController
   
   def locations
     #@locations = Location.most_common_program_locations(params[:q] || '')
-    @locations = Location.most_common_locations(params[:q] || '')
+    if params[:transfer_type].blank? || params[:transfer_type].nil?
+        @locations = Location.most_common_locations(params[:q] || '')
+    else
+        search = params[:q] || ''
+        location_tag_id = LocationTag.find_by_name("#{params[:transfer_type]}").id
+        location_ids = LocationTagMap.find(:all,:conditions => ["location_tag_id = (?)",location_tag_id]).map{|e|e.location_id}
+        @locations = Location.find(:all, :conditions=>["location.retired = 0 AND location_id IN (?) AND name LIKE ?", location_ids, "#{search}%"])
+    end
     @names = @locations.map do | location | 
       next if generic_locations.include?(location.name)
       "<li value='#{location.location_id}'>#{location.name}</li>" 
