c9ce3186b04e3e1a80e09b80db70469bc8fbed5d
mwatha <mwatha.bwanalie@baobabhealth.org>
2011-09-23 13:31:54 +0200

    working on TB-ART work flow

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1163,14 +1163,14 @@ EOF
   #from TB ART TO BART
   
   def tb_status
-    Concept.find(Observation.find(:last, 
+    Concept.find(Observation.find(:first, 
     :order => "obs_datetime DESC,date_created DESC",
     :conditions => ["person_id = ? AND concept_id = ?", self.id, 
     ConceptName.find_by_name("TB STATUS").concept_id]).value_coded).concept_names.map{|c|c.name}[0] rescue "UNKNOWN"
   end
   
   def hiv_status
-    status = Concept.find(Observation.find(:last, 
+    status = Concept.find(Observation.find(:first, 
     :order => "obs_datetime DESC,date_created DESC",
     :conditions => ["person_id = ? AND concept_id = ?", self.id, 
     ConceptName.find_by_name("HIV STATUS").concept_id]).value_coded).concept_names.map{|c|c.name}[0] rescue "UNKNOWN"
