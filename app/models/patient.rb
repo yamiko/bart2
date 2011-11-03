2ffa981f14ae3deadc2224be7ff89c0a58c084d7
Catchit <precious.bondwe@baobabhealth.org>
2011-11-03 16:04:15 +0200

    code cleaning

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1269,7 +1269,7 @@ EOF
    Encounter.find(:last,:conditions =>["encounter_type = ? and patient_id = ?",
         EncounterType.find_by_name("LAB RESULTS").id,self.id]).observations.map{|o| o } rescue nil
   end
-
+=begin
   def patient_need_sputum_test?
     encounter_date = Encounter.find(:last,
                       :conditions => ["encounter_type = ? and patient_id = ?",
@@ -1340,5 +1340,5 @@ EOF
         self.id, sputum_concept_ids, registration_date],
       :order => "obs_datetime desc", :limit => 3)
   end
-
+=end
 end
