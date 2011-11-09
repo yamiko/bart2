9ddcca97e4ad3597c5020af40088f89e906bb3b1
Catchit <precious.bondwe@baobabhealth.org>
2011-11-09 11:01:09 +0200

    moved child_bearing_femail, transfer_in?, transfer_in_date? from patient model

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -387,6 +387,10 @@ class EncountersController < ApplicationController
     # use @patient_tb_status  for the tb_status moved from the patient model
     @patient_tb_status = patient_tb_status(@patient)
 
+    @patient_is_transfer_in = transfer_in?(@patient)
+    @patient_transfer_in_date = transfer_in_date?(@patient)
+    @patient_is_child_bearing_female = child_bearing_female(@patient)
+
     	@cell_number = @patient.person.person_attributes.find_by_person_attribute_type_id(PersonAttributeType.find_by_name("Cell Phone Number").id).value rescue ''
 
     	@tb_symptoms = []
@@ -1004,6 +1008,21 @@ class EncountersController < ApplicationController
       return true if drug_order.drug.tb_medication?
     }
     false
+  end
+
+  def transfer_in_date?(patient)
+    patient_transfer_in = patient.person.observations.recent(1).question("HAS TRANSFER LETTER").all rescue nil
+    return patient_transfer_in.each{|datetime| return datetime.obs_datetime  if datetime.obs_datetime}
+  end
+
+  def transfer_in?(patient)
+    patient_transfer_in = patient.person.observations.recent(1).question("HAS TRANSFER LETTER").all rescue nil
+    return false if patient_transfer_in.blank?
+    return true
+  end
+
+  def child_bearing_female?(patient)
+    (gender == "Female" && patient.person.age >= 9 && patient.person.age <= 45) ? true : false
   end
 
 end
