3081a5aa8fafb2f4e557e9215595409d1bbd6f2f
Deliwe <dbnkhoma@gmail.com>
2011-11-17 08:51:37 +0200

    added PatientService in people_controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -188,7 +188,7 @@ class PeopleController < ApplicationController
     #then we create person from remote machine
 
     if create_from_remote
-      person_from_remote = create_remote_person(params)
+      person_from_remote = PatientService.create_remote_person(params)
       person = PatientService.create_from_form(person_from_remote["person"]) unless person_from_remote.blank?
 
       if !person.blank?
