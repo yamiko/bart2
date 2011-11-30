f1d5bcf3f4184c4bddf6fb1cc912c159c47d7d16
Deliwe <dbnkhoma@gmail.com>
2011-11-30 09:31:32 +0200

    updated patient functional test

--- test/functional/patient_controller_test.rb
+++ test/functional/patient_controller_test.rb
@@ -8,7 +8,8 @@ class PatientsControllerTest < ActionController::TestCase
   def setup  
     @controller = PatientsController.new
     @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new    
+    @response   = ActionController::TestResponse.new
+    @patient = patient(:evan)  
   end
 
   context "Patient controller" do
@@ -23,16 +24,6 @@ class PatientsControllerTest < ActionController::TestCase
       should "not show the pre art number if there is one and we are on the right location" do
         logged_in_as :mikmck, :registration do
           GlobalProperty.create(:property => 'dashboard.identifiers', :property_value => "{\"#{Location.current_location.id}\":[\"Pre ART Number\"]}")
-          get :show, {:id => patient(:evan).id}
-          assert_no_match /Pre ART Number/, @response.body
-          assert_no_match /PART\-311/, @response.body
-          assert_response :success
-        end  
-      end
-
-      should "not show the pre art number if there is one and we are on the wrong location" do
-        logged_in_as :mikmck, :registration do
-          GlobalProperty.create(:property => 'dashboard.identifiers', :property_value => "{\"#{Location.current_location.id+1}\":[\"Pre ART Number\"]}")
           get :show, {:id => patient(:evan).id}
           assert_no_match /Pre ART Number/, @response.body
           assert_no_match /PART\-311/, @response.body
@@ -59,9 +50,10 @@ class PatientsControllerTest < ActionController::TestCase
 
       should "get the mastercard_modify" do
         logged_in_as :mikmck, :registration do
-          #TODO rewrite the test
           get :mastercard_modify, {:id => patient(:evan).patient_id, :field => 'guardian'}
           assert_redirected_to("relationships/search?patient_id=#{patient(:evan).patient_id}")
+          get :mastercard_modify, {:id => patient(:evan).patient_id, :field => 'occupation'}
+          assert_response :success
         end
       end
 
@@ -72,6 +64,127 @@ class PatientsControllerTest < ActionController::TestCase
           assert_response :success
         end
       end
+
+      should "get the index view and show all necessary patient information" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :index, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+          assert_equal @response.body.include?("ARV-311"), true
+          assert_equal @response.body.include?("HIV Status"), true
+        end
+      end
+
+      should "relationships" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :relationships, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+      end    
+    
+     should "guardians" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :guardians, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+      end
+
+    should "get the patient graph" do
+      logged_in_as :mikmck, :registration do
+       #TODO rewrite the test
+       patient = patient(:evan)
+       c_weight = PatientService.get_patient_attribute_value(patient, "current_weight")
+       get :graph, {:patient_id => patient.patient_id, :data => "weight",
+                    :current_weight => c_weight}
+       assert_response :success
+      end
+    end
+
+    should "mastercard" do
+      logged_in_as :mikmck, :registration do
+       #TODO rewrite the test
+       patient = patient(:evan)
+       get :mastercard_printable, {:patient_id => patient.patient_id}
+       assert_response :success
+      end
+    end
+
+    should "print mastercard" do
+      logged_in_as :mikmck, :registration do
+       #TODO rewrite the test
+       patient = patient(:evan)
+       get :print_mastercard, {:patient_id => patient.patient_id}
+       assert_redirected_to("patients/mastercard?patient_id=#{patient(:evan).patient_id}")
+      end
+    end    
+
+   should "demographics" do
+      logged_in_as :mikmck, :registration do
+       #TODO rewrite the test
+       patient = patient(:evan)
+       get :demographics, {:patient_id => patient.patient_id}
+       assert_response :success
+      end
+    end
+
+    should "overview" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :overview, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+    end
+      
+    should "visit_history" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :visit_history, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+    end
+
+    should "past_visits_summary" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :past_visits_summary, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+    end
+
+    should "treatment_dashboard" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :treatment_dashboard, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+    end
+
+    should "guardians_dashboard" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :guardians_dashboard, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+    end
+
+   should "programs_dashboard" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :programs_dashboard, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+    end
+
+    should "recent_lab_orders" do
+        logged_in_as :mikmck, :registration do
+          #TODO rewrite the test
+          get :recent_lab_orders, {:patient_id => patient(:evan).patient_id}
+          assert_response :success
+        end
+    end
+           
     end
   end
 end
