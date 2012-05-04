6958e15ee7cac42a7bdd9f450f1d0e405351d7b5
Yamiko <yamikomsosa@yahoo.co.uk>
2012-05-04 03:19:17 +0200

    Removed absolute db refs in views & routines schema modifications script and added related entry to init script

--- script/initial_database_setup.sh
+++ script/initial_database_setup.sh
@@ -33,6 +33,7 @@ echo "CREATE DATABASE $DATABASE;" | mysql --user=$USERNAME --password=$PASSWORD
 mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/openmrs_1_7_2_concept_server_full_db.sql
 #echo "schema additions"
 mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/schema_bart2_additions.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/bart2_views_schema_additions.sql
 #echo "defaults"
 mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/defaults.sql
 #echo "user schema modifications"
