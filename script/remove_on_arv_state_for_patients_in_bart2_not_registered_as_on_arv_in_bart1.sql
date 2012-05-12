65ed2bc7cb57af08309555fd7b9f46283edcf453
Catchit <precious.bondwe@baobabhealth.org>
2012-05-12 17:07:39 +0200

    used joins and made few modifications

--- script/remove_on_arv_state_for_patients_in_bart2_not_registered_as_on_arv_in_bart1.sql
+++ script/remove_on_arv_state_for_patients_in_bart2_not_registered_as_on_arv_in_bart1.sql
@@ -6,9 +6,12 @@ WHERE patient_program_id IN (
                             SELECT patient_program_id
                             FROM bart2.patient_program bpp
                             INNER JOIN (SELECT p.patient_id
-                                        FROM bart2.patient_program pp, bart2.patient_state ps, patient p 
-                                        WHERE pp.patient_program_id = ps.patient_program_id 
-                                            AND ps.state =7 AND p.patient_id = pp.patient_id 
+                                        FROM bart2.patient_program pp 
+                                             INNER JOIN bart2.patient_state ps
+                                                 ON pp.patient_program_id = ps.patient_program_id
+                                             INNER JOIN bart2.patient p 
+                                                ON p.patient_id = pp.patient_id 
+                                        WHERE ps.state =7 
                                             AND p.voided=0 AND pp.voided=0 AND ps.voided=0 
                                             AND p.date_created < @'set_date' AND ps.start_date < @'set_date' 
                                             AND p.patient_id NOT IN (SELECT r.patient_id 
@@ -26,17 +29,20 @@ SET voided = 1
 WHERE encounter_id IN ( SELECT b2e.encounter_id
                         FROM bart2.encounter b2e
                         INNER JOIN (SELECT p.patient_id
-                                    FROM bart2.patient_program pp, bart2.patient_state ps, patient p 
-                                    WHERE pp.patient_program_id = ps.patient_program_id 
-                                        AND ps.state =7 AND p.patient_id = pp.patient_id 
-                                        AND p.voided=0 AND pp.voided=0 AND ps.voided=0 
-                                        AND p.date_created < @'set_date' AND ps.start_date < @'set_date'
-                                        AND p.patient_id NOT IN (SELECT r.patient_id 
-                                                                 FROM bart1.patient_registration_dates r, patient p 
-                                                                 WHERE r.patient_id = p.patient_id 
-                                                                 AND p.date_created < @'set_date')
-                                    GROUP BY patient_id) pat
-                            ON b2e.encounter_id = pat.patient_id
+                                        FROM bart2.patient_program pp 
+                                             INNER JOIN bart2.patient_state ps
+                                                 ON pp.patient_program_id = ps.patient_program_id
+                                             INNER JOIN bart2.patient p 
+                                                ON p.patient_id = pp.patient_id 
+                                        WHERE ps.state =7 
+                                            AND p.voided=0 AND pp.voided=0 AND ps.voided=0 
+                                            AND p.date_created < @'set_date' AND ps.start_date < @'set_date' 
+                                            AND p.patient_id NOT IN (SELECT r.patient_id 
+                                                                     FROM bart1.patient_registration_dates r, patient p 
+                                                                     WHERE r.patient_id = p.patient_id 
+                                                                     AND p.date_created < @'set_date')
+                                        GROUP BY patient_id) pat
+                            ON b2e.patient_id = pat.patient_id
                         WHERE b2e.encounter_type = 54 AND b2e.encounter_datetime < @'set_date');
 
 
@@ -44,9 +50,12 @@ WHERE encounter_id IN ( SELECT b2e.encounter_id
 UPDATE bart2.encounter
 SET voided = 1
 WHERE patient_id IN (   SELECT p.patient_id
-                        FROM bart2.patient_program pp, bart2.patient_state ps, patient p 
-                        WHERE pp.patient_program_id = ps.patient_program_id 
-                            AND ps.state =7 AND p.patient_id = pp.patient_id 
+                        FROM bart2.patient_program pp 
+                             INNER JOIN bart2.patient_state ps
+                                 ON pp.patient_program_id = ps.patient_program_id
+                             INNER JOIN bart2.patient p 
+                                ON p.patient_id = pp.patient_id 
+                        WHERE ps.state =7 
                             AND p.voided=0 AND pp.voided=0 AND ps.voided=0 
                             AND p.date_created < @'set_date' AND ps.start_date < @'set_date' 
                             AND p.patient_id NOT IN (SELECT r.patient_id 
