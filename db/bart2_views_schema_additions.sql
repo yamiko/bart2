ff3e8dd559881ab9cb2c6ef539f2daa01fad970d
Yamiko <yamikomsosa@yahoo.co.uk>
2012-05-21 12:55:36 +0200

    Refined current defaulter method

--- db/bart2_views_schema_additions.sql
+++ db/bart2_views_schema_additions.sql
@@ -368,14 +368,15 @@ BEGIN
   	DECLARE my_daily_dose, my_quantity INT;
 	DECLARE flag INT;
 
-  	DECLARE cur1 CURSOR FOR SELECT o.start_date, d.equivalent_daily_dose daily_dose, d.quantity, obs.obs_datetime, MAX(obs.obs_datetime) AS latest_date FROM drug_order d
+  	DECLARE cur1 CURSOR FOR SELECT o.start_date, d.equivalent_daily_dose daily_dose, d.quantity, obs.obs_datetime, DATE(MAX(obs.obs_datetime)) AS latest_date FROM drug_order d
     						LEFT JOIN orders o ON d.order_id = o.order_id
     						LEFT JOIN obs ON d.order_id = obs.order_id
     					WHERE d.drug_inventory_id IN (SELECT drug_id FROM drug WHERE concept_id IN (SELECT concept_id FROM concept_set WHERE concept_set = 1085)) 
         					AND quantity > 0
        						AND obs.concept_id = 2834
                   			AND obs.voided = 0
-						    AND obs.person_id = my_patient_id;
+						    AND obs.person_id = my_patient_id
+						HAVING DATE(obs.obs_datetime) = latest_date;
 
   	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
@@ -391,18 +392,16 @@ BEGIN
 			IF done THEN
 				CLOSE cur1;
 				LEAVE read_loop;
-				END IF;
+			END IF;
 				
-			IF DATE(my_obs_datetime) = DATE(my_latest_date) THEN
-				SET @expiry_date = ADDDATE(my_start_date, (my_quantity/my_daily_dose));
+			SET @expiry_date = ADDDATE(my_start_date, (my_quantity/my_daily_dose));
 
-				IF my_expiry_date IS NULL THEN
-					SET my_expiry_date = @expiry_date;
-				END IF;
+			IF my_expiry_date IS NULL THEN
+				SET my_expiry_date = @expiry_date;
+			END IF;
 
-	 			IF  @expiry_date < my_expiry_date THEN
-					SET my_expiry_date = @expiry_date;      
-					END IF;
+ 			IF  @expiry_date < my_expiry_date THEN
+				SET my_expiry_date = @expiry_date;      
 			END IF;
 		END LOOP;
 
