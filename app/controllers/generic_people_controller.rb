86053afa4df5162aa71409ee6b1bce703fe5c065
yamiko <yamikomsosa@yahoo.co.uk>
2012-08-29 20:08:57 +0200

    Fixed show confirmation option to accept both true and false

--- app/controllers/generic_people_controller.rb
+++ app/controllers/generic_people_controller.rb
@@ -120,16 +120,16 @@ class GenericPeopleController < ApplicationController
 		@found_person_id = params[:found_person_id] 
 		@relation = params[:relation]
 		@person = Person.find(@found_person_id) rescue nil
-    @current_hiv_program_state = PatientProgram.find(:first, :joins => :location, :conditions => ["program_id = ? AND patient_id = ? AND location.location_id = ?", Program.find_by_concept_id(Concept.find_by_name('HIV PROGRAM').id).id,@person.patient, Location.current_health_center]).patient_states.last.program_workflow_state.concept.fullname rescue ''
-    @transferred_out = @current_hiv_program_state.upcase == "PATIENT TRANSFERRED OUT"? true : nil
-    defaulter = Patient.find_by_sql("SELECT current_defaulter(#{@person.patient.patient_id}, '#{session_date}') 
+		@current_hiv_program_state = PatientProgram.find(:first, :joins => :location, :conditions => ["program_id = ? AND patient_id = ? AND location.location_id = ?", Program.find_by_concept_id(Concept.find_by_name('HIV PROGRAM').id).id,@person.patient, 				Location.current_health_center]).patient_states.last.program_workflow_state.concept.fullname rescue ''
+    	@transferred_out = @current_hiv_program_state.upcase == "PATIENT TRANSFERRED OUT"? true : nil
+		defaulter = Patient.find_by_sql("SELECT current_defaulter(#{@person.patient.patient_id}, '#{session_date}') 
                                      AS defaulter 
                                      FROM patient_program LIMIT 1")[0].defaulter rescue 0
-    @defaulted = "#{defaulter}" == "0" ? nil : true     
-    @task = main_next_task(Location.current_location, @person.patient, session_date.to_date)
+    	@defaulted = "#{defaulter}" == "0" ? nil : true     
+    	@task = next_task(@person.patient)
 		@arv_number = PatientService.get_patient_identifier(@person, 'ARV Number')
 		@patient_bean = PatientService.get_patient(@person)                                                             
-    render :layout => false	
+    	render :layout => false	
 	end
 
 	def tranfer_patient_in
