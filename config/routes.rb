651eb49ae27216d971f87939832bb609c9a2c6e3
Mwatha Bwanali <mwathabwanali@gmail.com>
2012-09-20 16:56:06 +0200

    allowing bart2 to assign ART activities to users from ANC

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ ActionController::Routing::Routes.draw do |map|
 	map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 	map.location '/location', :controller => 'sessions', :action => 'location'
 	map.encounter '/encounters/new/:encounter_type', :controller => 'encounters', :action => 'new'  
+	map.encounter '/encounters/new/:encounter_type/:id', :controller => 'encounters', :action => 'new'  
 	map.resource :session
 	map.resources :dispensations, :collection => {:quantities => :get}
 	map.resources :barcodes, :collection => {:label => :get}
