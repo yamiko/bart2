ceabe4ea847f5e65a34da3816affd36a0c9c5a52
Catchit <precious.bondwe@googlemail.com>
2012-10-02 21:41:43 +0200

    fixed errors that occured during execution

--- script/move_value_text_to_value_datetime_datearvstarted.rb
+++ script/move_value_text_to_value_datetime_datearvstarted.rb
@@ -1,16 +1,19 @@
 puts "\nStart Time: #{Time.now}"
 
+failure = File.open("failure_id.DAT", 'w')
+
 concept_id = Concept.find_by_name("Date antiretrovirals started").id
 
 obs=Observation.find(:all,
-                     :conditions => ["concept_id = ? AND value_text IS NOT NULL",
+                     :conditions => ["concept_id = ? AND value_text IS NOT NULL AND value_text <> ''",
                                      concept_id])
 
 obs.each do |o|
-    o.value_datetime = o.value_text.to_date
+    o.value_datetime = o.value_text
     if o.save
         puts "\n Obs #{o.obs_id} >> successfull"
     else
+	failure.puts o.obs_id		
         puts "\n Obs #{o.obs_id} >> failed"
     end
 end
