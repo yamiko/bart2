1aef39b0afe3db769d0ebe5a0b98ba976079314a
Fchirwa <F88kavutausiwa@gmail.com>
2013-01-24 09:45:36 +0200

    optimised cohort for mpc

---
+++ config/initializers/connections_mysql.rb
@@ -0,0 +1,19 @@
+unless Rails.env.to_s == 'test'
+  module ActiveRecord::ConnectionAdapters
+    class Mysql2Adapter
+      alias_method :execute_without_retry, :execute
+
+    def execute(*args)
+      execute_without_retry(*args)
+      rescue Exception => e
+        if e.message =~ /server has gone away/i
+          warn "Server timed out, retrying"
+          reconnect!
+          retry
+        else
+          raise e
+        end
+      end
+    end
+  end
+end
