d796d861d9b7c725a081c2d8d95bd8764e67f324
Fchirwa <F88kavutausiwa@gmail.com>
2013-02-25 12:31:24 +0200

    setting up for normal staging

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -383,7 +383,7 @@ class EncountersController < GenericEncountersController
 		redirect_to :action => :create, 'encounter[encounter_type_name]' => params[:encounter_type].upcase, 'encounter[patient_id]' => @patient.id and return if ['registration'].include?(params[:encounter_type])
 		
 		if (params[:encounter_type].upcase rescue '') == 'HIV_STAGING' and  (CoreService.get_global_property_value('use.extended.staging.questions').to_s == "true" rescue false)
-			render :template => 'encounters/extended_hiv_staging'
+			render :template => 'encounters/normal_hiv_staging'
 		else
 			render :action => params[:encounter_type] if params[:encounter_type]
 		end
