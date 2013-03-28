da9e570c1cc93f638c4fdc6cde14c9c9a5a52f3f
Fchirwa <F88kavutausiwa@gmail.com>
2013-03-28 14:30:11 +0200

    fixed tb states

--- app/controllers/generic_programs_controller.rb
+++ app/controllers/generic_programs_controller.rb
@@ -108,25 +108,6 @@ class GenericProgramsController < ApplicationController
     render :text => @names.join('')  
   end
 
-	  def tb_states
-		tb_states = ["currently in treatment", "patient cured", "patient transferred out", "regimen failure","treatment complete","patient died"]
-    if params[:show_non_terminal_states_only].to_s == true.to_s
-       @states = ProgramWorkflowState.all(:conditions => ['program_workflow_id = ? AND terminal = 0', params[:workflow]], :include => :concept)
-    else
-       @states = ProgramWorkflowState.all(:conditions => ['program_workflow_id = ?', params[:workflow]], :include => :concept)
-    end
-
-    @names = @states.map{|state|
-
-      name = state.concept.concept_names.typed("SHORT").first.name rescue state.concept.fullname
-      next if name.blank?
-			if tb_states.include?(name.downcase.to_s)
-				 "<li value='#{state.id}'>#{name}</li>" unless name == params[:current_state]
-			end
-    }
-    render :text => @names.join('')
-  end
-
   def update
     flash[:error] = nil
     
