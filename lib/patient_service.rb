4ba222ce56b5f6ca8bd246ccffb7105dfcae9fa9
blessings <blessings.mhango@gmail.com>
2013-04-19 11:58:43 +0200

    defaulted label printing to two

--- lib/patient_service.rb
+++ lib/patient_service.rb
@@ -686,7 +686,7 @@ module PatientService
   	services = Observation.find(:all, :conditions => ["DATE(date_created) = ? AND concept_id = ?", Date.today.to_date, ConceptName.find_by_name("SERVICES").concept_id], :order => "obs_datetime desc")
   end
 
-  def self.patient_national_id_label(patient)
+  def self.patient_national_id_label(patient,copies = 2)
 	  patient_bean = get_patient(patient.person)
     return unless patient_bean.national_id
     sex =  patient_bean.sex.match(/F/i) ? "(F)" : "(M)"
@@ -707,7 +707,7 @@ module PatientService
     label.draw_multi_text("#{patient_bean.name.titleize}")
     label.draw_multi_text("#{patient_bean.national_id_with_dashes} #{patient_bean.birth_date}#{sex}")
     label.draw_multi_text("#{address}" ) unless address.blank?
-    label.print(1)
+    label.print(copies)
   end
 
   def self.recent_sputum_submissions(patient_id)
