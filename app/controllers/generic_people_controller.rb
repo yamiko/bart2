148a5f98bcb176cc20a3d548855ac55301480104
Ernest Matola <mangochiman@gmail.com>
2013-05-02 14:57:22 +0200

    pulling the second line art start date for patient

--- app/controllers/generic_people_controller.rb
+++ app/controllers/generic_people_controller.rb
@@ -243,10 +243,12 @@ class GenericPeopleController < ApplicationController
 		@patient_bean = PatientService.get_patient(@person)  
 		
 		
-		@art_start_date = PatientService.date_antiretrovirals_started(@person.patient) 
+		@art_start_date = PatientService.date_antiretrovirals_started(@person.patient)
+    @second_line_treatment_start_date = PatientService.date_started_second_line_regimen(@person.patient) rescue nil
+    @duration_in_months = PatientService.period_on_treatment(@art_start_date) rescue nil
 		#@duration_in_months = ((Time.now.to_date - @art_start_date.to_date).to_i/28) unless @art_start_date.blank?
     patient = @person.patient
-		@duration_in_months = PatientService.period_on_treatment(@art_start_date) rescue nil
+		@second_line_duration_in_months = PatientService.period_on_treatment(@second_line_treatment_start_date) rescue nil
     @identifier_types = ["Legacy Pediatric id","National id","Legacy National id"]
 			identifier_types = PatientIdentifierType.find(:all,                         
 			  :conditions=>["name IN (?)",@identifier_types]                              
