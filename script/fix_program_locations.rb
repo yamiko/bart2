373bd8f18887a9f9b214c3bf4f911a0a4835733d
timmtonga <timmtonga@gmail.com>
2013-06-13 08:13:55 +0200

    added exception for missing date of birth

--- script/fix_program_locations.rb
+++ script/fix_program_locations.rb
@@ -13,17 +13,20 @@ def fix_retired_drug
 	retired.each do |drug_order|
 	
 		dispense_date = drug_order.order.start_date.to_date.year
-		dob = drug_order.order.encounter.patient.person.birthdate.to_date.year
+		dob = drug_order.order.encounter.patient.person.birthdate.to_date.year rescue nil
 	
-		age = dispense_date - dob
+		unless dob.nil?
+
+			age = dispense_date - dob
 		
-		if age < 14
-			drug_order.drug_inventory_id = Drug.find_by_name('AZT/3TC/NVP (60/30/50mg tablet)').id
-		else
-			drug_order.drug_inventory_id = Drug.find_by_name('AZT/3TC/NVP (300/150/200mg tablet)').id		
+			if age < 14
+				drug_order.drug_inventory_id = Drug.find_by_name('AZT/3TC/NVP (60/30/50mg tablet)').id
+			else
+				drug_order.drug_inventory_id = Drug.find_by_name('AZT/3TC/NVP (300/150/200mg tablet)').id		
+			end
+				
+				drug_order.save!
 		end
-	
-			drug_order.save!
 	end 
 
 end
