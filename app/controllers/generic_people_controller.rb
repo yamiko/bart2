299d12d58fd3bb4068b9adb981edd77db976e18c
Mwatha Bwanali <mwathabwanali@gmail.com>
2013-07-30 15:43:23 +0200

    adding footprints table structure changes

--- app/controllers/generic_people_controller.rb
+++ app/controllers/generic_people_controller.rb
@@ -213,12 +213,12 @@ class GenericPeopleController < ApplicationController
           redirect_to search_complete_url(found_person.id, params[:relation]) and return
         elsif national_id_replaced.to_s == "true"
           #creating patient's footprint so that we can track them later when they visit other sites
-          DDEService.create_footprint(PatientService.get_patient(found_person).national_id, Location.find(session[:location_id]).name)
+          DDEService.create_footprint(PatientService.get_patient(found_person).national_id, "BART2 - #{BART_VERSION}")
           print_and_redirect("/patients/national_id_label?patient_id=#{found_person.id}", next_task(found_person.patient)) and return
           redirect_to :action => 'confirm', :found_person_id => found_person.id, :relation => params[:relation] and return
         else
           #creating patient's footprint so that we can track them later when they visit other sites
-          DDEService.create_footprint(PatientService.get_patient(found_person).national_id, Location.find(session[:location_id]).name)
+          DDEService.create_footprint(PatientService.get_patient(found_person).national_id, "BART2 - #{BART_VERSION}")
           redirect_to :action => 'confirm',:found_person_id => found_person.id, :relation => params[:relation] and return
         end
       end
@@ -436,11 +436,11 @@ class GenericPeopleController < ApplicationController
 					end
 
           #creating patient's footprint so that we can track them later when they visit other sites
-          DDEService.create_footprint(PatientService.get_patient(person).national_id, Location.find(session[:location_id]).name)
+          DDEService.create_footprint(PatientService.get_patient(person).national_id, "BART2 - #{BART_VERSION}")
           print_and_redirect("/patients/national_id_label?patient_id=#{person.id}", next_task(person.patient)) and return
         end
         #creating patient's footprint so that we can track them later when they visit other sites
-        DDEService.create_footprint(PatientService.get_patient(person).national_id, Location.find(session[:location_id]).name)
+        DDEService.create_footprint(PatientService.get_patient(person).national_id, "BART2 - #{BART_VERSION}")
       end
       redirect_to search_complete_url(params[:person][:id], params[:relation]) and return unless params[:person][:id].blank? || params[:person][:id] == '0'
 
@@ -571,7 +571,7 @@ class GenericPeopleController < ApplicationController
 
     #If we are creating from DDE then we must create a footprint of the just created patient to
     #enable future
-    DDEService.create_footprint(PatientService.get_patient(person).national_id, Location.find(session[:location_id]).name)
+    DDEService.create_footprint(PatientService.get_patient(person).national_id, "BART2 - #{BART_VERSION}")
     
 
     #for now BART2 will use BART1 for patient/person creation until we upgrade BART1 to 2
