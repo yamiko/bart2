cfa49ec7b89e8c7d61d8821440b4c558b11d28c1
timmtonga <timmtonga@gmail.com>
2013-09-20 16:00:35 +0200

    worked on migration completion scripts

--- script/temp_encounter_migrator.rb
+++ script/temp_encounter_migrator.rb
@@ -5,17 +5,18 @@ $mysql_conn = Mysql.new(ARGV[0], ARGV[1], ARGV[2],ARGV[3])
 def start
 
   encounters = $mysql_conn.query("SELECT * FROM temp_encounter")
-
+  count = encounters.num_rows
+  puts "#{count} encounters to be migrated"
   (encounters || []).each do |encounter|
-
+    puts "#{count} encounters to go"
     Encounter.transaction do
       new_encounter = Encounter.new()
       new_encounter.encounter_type = encounter[1]
-      new_encounter.patient_id = encounter[2]
-      new_encounter.provider = encounter[3]
-      new_encounter.location_id = encounter[4]
+      new_encounter.patient_id = encounter[2].to_i
+      new_encounter.provider = Person.find(encounter[3].to_i)
+      new_encounter.location_id = encounter[4].to_i
       new_encounter.encounter_datetime = encounter[6]
-      new_encounter.creator = encounter[7]
+      new_encounter.creator = encounter[7].to_i
       new_encounter.uuid = encounter[13]
       if new_encounter.save
 
@@ -23,27 +24,27 @@ def start
 
         (observations || []).each do |obs|
           new_obs = Observation.new()
-          new_obs.person_id = obs[1]
-          new_obs.concept_id = obs[2]
-          new_obs.encounter_id = obs[3]
-          new_obs.order_id = obs[4]
+          new_obs.person_id = obs[1].to_i
+          new_obs.concept_id = obs[2].to_i
+          new_obs.encounter_id = new_encounter.id
+          new_obs.order_id = (obs[4].to_i == 0) ? nil : obs[4].to_i
           new_obs.obs_datetime = obs[5]
-          new_obs.location_id = obs[6]
+          new_obs.location_id = obs[6].to_i
           new_obs.obs_group_id = obs[7]
           new_obs.accession_number = obs[8]
           new_obs.value_group_id = obs[9]
           new_obs.value_boolean = obs[10]
-          new_obs.value_coded = obs[11]
+          new_obs.value_coded =  (obs[11].to_i == 0) ? nil : obs[11].to_i
           new_obs.value_coded_name_id = obs[12]
-          new_obs.value_drug = obs[13]
+          new_obs.value_drug =  (obs[13].to_i == 0) ? nil : obs[13].to_i
           new_obs.value_datetime = obs[14]
-          new_obs.value_numeric = obs[15]
+          new_obs.value_numeric =  (obs[15].to_i == 0) ? nil : obs[15].to_i
           new_obs.value_modifier = obs[16]
           new_obs.value_text = obs[17]
           new_obs.date_started = obs[18]
           new_obs.date_stopped = obs[19]
           new_obs.comments = obs[20]
-          new_obs.creator = obs[21]
+          new_obs.creator = obs[21].to_i
           new_obs.date_created = obs[22]
           new_obs.voided = obs[23]
           new_obs.voided_by = obs[24]
@@ -56,7 +57,7 @@ def start
 
       end
     end
-
+    count -=1
   end
 
 end
