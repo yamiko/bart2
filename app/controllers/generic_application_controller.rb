9a9cdc420cd6c2527298aa843cc3edd118a0fb97
Ernest Matola <mangochiman@gmail.com>
2014-01-07 16:34:14 +0200

    Skipping authentication of art_summary_dispensation method

--- app/controllers/generic_application_controller.rb
+++ app/controllers/generic_application_controller.rb
@@ -26,17 +26,23 @@ class GenericApplicationController < ActionController::Base
 	filter_parameter_logging :password
 	before_filter :authenticate_user!, :except => ['login', 'logout','remote_demographics',
 		                                      'create_remote', 'mastercard_printable', 'get_token',
-		                                      'cohort','demographics_remote', 'export_on_art_patients', 'art_summary']
+		                                      'cohort','demographics_remote', 'export_on_art_patients', 'art_summary',
+                                          'art_summary_dispensation'
+                                          ]
 
     before_filter :set_current_user, :except => ['login', 'logout','remote_demographics',
 		                                      'create_remote', 'mastercard_printable', 'get_token',
-                                          'cohort','demographics_remote', 'export_on_art_patients', 'art_summary']
+                                          'cohort','demographics_remote', 'export_on_art_patients', 'art_summary',
+                                          'art_summary_dispensation'
+                                          ]
 
 	before_filter :location_required, :except => ['login', 'logout', 'location',
 		                                        'demographics','create_remote',
 		                                         'mastercard_printable',
 		                                        'remote_demographics', 'get_token',
-                                            'cohort','demographics_remote', 'export_on_art_patients', 'art_summary']
+                                            'cohort','demographics_remote', 'export_on_art_patients', 'art_summary',
+                                            'art_summary_dispensation'
+                                            ]
 
 	before_filter :set_return_uri
   
