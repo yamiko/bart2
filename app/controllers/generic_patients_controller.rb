1384d68bc38f11d0c363069cefb13b90f8ebe82b
Ernest Matola <mangochiman@gmail.com>
2014-01-13 20:44:30 +0200

    Working on merging patients

--- app/controllers/generic_patients_controller.rb
+++ app/controllers/generic_patients_controller.rb
@@ -3524,16 +3524,18 @@ end
   end
 
   def search_all
-    search_str = params[:search_str] ; side = params[:side]
+    search_str = params[:search_str]
+    side = params[:side]
     search_by_identifier = search_str.match(/[0-9]+/).blank? rescue false
 
     unless search_by_identifier
       patients = PatientIdentifier.find(:all, :conditions => ["voided = 0 AND (identifier LIKE ?)",
       "%#{search_str}%"],:limit => 10).map{| p |p.patient}
     else
-      given_name = search_str.split(' ')[0] rescue '' ; family_name = search_str.split(' ')[1] rescue ''
-      patients = PersonName.find(:all ,:joins => [:person], :conditions => ["person.voided = 0 AND family_name LIKE ? AND given_name LIKE ?",
-      "#{family_name}%","%#{given_name}%"],:limit => 10)
+      given_name = search_str.split(' ')[0] rescue ''
+      family_name = search_str.split(' ')[1] rescue ''
+      patients = PersonName.find(:all ,:joins => [:person => [:patient]], :conditions => ["person.voided = 0 AND family_name LIKE ? AND given_name LIKE ?",
+      "#{family_name}%","%#{given_name}%"],:limit => 10).collect{|pn|pn.person.patient}
     end
     @html = <<EOF
 <html>
@@ -3557,27 +3559,33 @@ end
 EOF
 
       color = 'blue'
-      patients.each do |person_name|
-        next if person_name.person.addresses.blank?
-        next if person_name.person.patient.blank?
+      patients.each do |patient|
+        next if patient.person.blank?
+        next if patient.person.addresses.blank?
         if color == 'blue'
           color = 'white'
         else
           color='blue'
         end
-        bean = PatientService.get_patient(person_name.person)
+        bean = PatientService.get_patient(patient.person)
+        total_encounters = patient.encounters.count rescue nil
+        latest_visit = patient.encounters.last.encounter_datetime.strftime("%a, %d-%b-%y") rescue nil
         @html+= <<EOF
 <tr>
-  <td class='color_#{color} patient_#{person_name.person.id}' style="text-align:left;" onclick="setPatient('#{person_name.person.id}','#{color}','#{side}')">Name:&nbsp;#{bean.name || '&nbsp;'}</td>
-  <td class='color_#{color} patient_#{person_name.person.id}' style="text-align:left;" onclick="setPatient('#{person_name.person.id}','#{color}','#{side}')">Age:&nbsp;#{bean.age || '&nbsp;'}</td>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">Name:&nbsp;#{bean.name || '&nbsp;'}</td>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">Age:&nbsp;#{bean.age || '&nbsp;'}</td>
 </tr>
 <tr>
-  <td class='color_#{color} patient_#{person_name.person.id}' style="text-align:left;" onclick="setPatient('#{person_name.person.id}','#{color}','#{side}')">Guardian:&nbsp;#{bean.guardian rescue '&nbsp;'}</td>
-  <td class='color_#{color} patient_#{person_name.person.id}' style="text-align:left;" onclick="setPatient('#{person_name.person.id}','#{color}','#{side}')">ARV number:&nbsp;#{bean.arv_number rescue '&nbsp;'}</td>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">Guardian:&nbsp;#{bean.guardian rescue '&nbsp;'}</td>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">ARV number:&nbsp;#{bean.arv_number rescue '&nbsp;'}</td>
 </tr>
 <tr>
-  <td class='color_#{color} patient_#{person_name.person.id}' style="text-align:left;" onclick="setPatient('#{person_name.person.id}','#{color}','#{side}')">National ID:&nbsp;#{bean.national_id rescue '&nbsp;'}</td>
-  <td class='color_#{color} patient_#{person_name.person.id}' style="text-align:left;" onclick="setPatient('#{person_name.person.id}','#{color}','#{side}')">TA:&nbsp;#{bean.home_district rescue '&nbsp;'}</td>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">National ID:&nbsp;#{bean.national_id rescue '&nbsp;'}</td>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">TA:&nbsp;#{bean.home_district rescue '&nbsp;'}</td>
+</tr>
+<tr>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">Total Encounters:&nbsp;#{total_encounters rescue '&nbsp;'}</td>
+  <td class='color_#{color} patient_#{patient.id}' style="text-align:left;" onclick="setPatient('#{patient.id}','#{color}','#{side}')">Latest Visit:&nbsp;#{latest_visit rescue '&nbsp;'}</td>
 </tr>
 EOF
       end
@@ -3596,7 +3604,7 @@ EOF
           Patient.merge(master,patient_id.to_i)
         end
       end
-      flash[:notice] = "Successfully merged patients"
+      #render :text => "showMessage('Successfully merged patients')" and return
     end
     redirect_to :action => "merge_menu" and return
   end
