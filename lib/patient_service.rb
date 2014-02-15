1d196d2395a3fa582b289f2febb998eed93cfea0
timmtonga <timmtonga@gmail.com>
2014-02-15 15:49:54 +0200

    added condition to enable estimated dates to be pushed

--- lib/patient_service.rb
+++ lib/patient_service.rb
@@ -208,6 +208,8 @@ module PatientService
         birthdate.strftime("??/%b/%Y")
       elsif birthdate.day == 1 and birthdate.month == 1
         birthdate.strftime("??/???/%Y")
+      else 
+	      birthdate.strftime("%d/%b/%Y") unless birthdate.blank?
       end
     else
       birthdate.strftime("%d/%b/%Y")
