503454a7c4dbe37f304452e7abf3bc2c7628fc97
Deliwe <dbnkhoma@gmail.com>
2014-02-18 16:11:03 +0200

    updated script used to initialize flat_tables

--- script/initial_flat_table_load.rb
+++ script/initial_flat_table_load.rb
@@ -9,7 +9,8 @@ def load_data
 	count = patients.length
 	puts "Number of patients to be initialized: #{patients.length}"
   return if patients.blank?
-	
+  
+  puts Date.today
 	patients.each do |patient|
 		puts "working on patient_id: #{patient.patient_id}......#{count} patients to go "
 		orders = []
@@ -24,11 +25,13 @@ def load_data
 			orders << order
 			drug_orders << order.drug_order unless order.drug_order.blank?
 		end
-		
+
 		load_orders(orders)			
 		load_drug_orders(drug_orders)
 		load_patient_identifiers(patient.id)	
 		load_observations(patient.id)
+		load_orders1(orders)			
+		load_drug_orders1(drug_orders)
 		count -=1	
 	end
 
@@ -49,15 +52,17 @@ def load_states(patient_id)
 end
 
 def load_orders(orders)
-
+  #raise orders.to_yaml
 	#puts "Loading orders. #{orders.length} orders"
   order_ids = []
 	orders.each do |order|
-    order_ids << order.id
+		#puts "#{order.order_id}"
+    order_ids << order.order_id
 	end		
-	
+
 	if !order_ids.blank?
 	 order_ids.each do |order|
+	 #puts "#{order}"
     ActiveRecord::Base.connection.execute <<EOF
       UPDATE orders SET voided = 0 WHERE order_id = #{order};
 EOF
@@ -67,6 +72,44 @@ EOF
 end
 
 def	load_drug_orders(drug_orders)
+
+	#puts "#{drug_orders.length} drug order sets"
+  drug_order_ids = []
+	drug_orders.each do |drug_order|
+    drug_order_ids << drug_order.id
+	end		
+  	
+  if !drug_order_ids.blank?
+    drug_order_ids.each do |drug_order|
+      	 #puts "#{drug_order}"
+      ActiveRecord::Base.connection.execute <<EOF
+        UPDATE drug_order SET complex = 0 WHERE order_id = #{drug_order};
+EOF
+    end
+  end
+end
+
+def load_orders1(orders)
+  #raise orders.to_yaml
+	#puts "Loading orders. #{orders.length} orders"
+  order_ids = []
+	orders.each do |order|
+		#puts "#{order.order_id}"
+    order_ids << order.order_id
+	end		
+
+	if !order_ids.blank?
+	 order_ids.each do |order|
+
+    ActiveRecord::Base.connection.execute <<EOF
+      UPDATE orders SET voided = 0 WHERE order_id = #{order};
+EOF
+   end
+  end
+		
+end
+
+def	load_drug_orders1(drug_orders)
 
 	#puts "#{drug_orders.length} drug order sets"
   drug_order_ids = []
@@ -94,23 +137,23 @@ end
 def load_observations(person_id)
 
 	observations = Observation.find(:all, :conditions => ["person_id = #{person_id}"])
-  obs_ids = observations rescue nil
+  obs_ids = observations.map(&:obs_id) rescue nil
   next if obs_ids.blank?
 
-  obs_ids.each do |obs|
+  #obs_ids.each do |obs|
     ActiveRecord::Base.connection.execute <<EOF
-      UPDATE obs SET voided = 0 WHERE obs_id = #{obs.obs_id};
+      UPDATE obs SET voided = 0 WHERE obs_id IN (#{obs_ids.join(',')});
 EOF
-end
-                                                                                
+#end
+                                                            
 end
 
-def get_art_patients                                                            
-  art_encounters = ['HIV CLINIC REGISTRATION','HIV RECEPTION','VITALS',         
+def get_art_patients         
+  art_encounters = ['HIV CLINIC REGISTRATION','HIV RECEPTION','VITALS',
     'HIV STAGING','HIV CLINIC CONSULTATION','ART ADHERENCE','TREATMENT','DISPENSING']
                                                                                 
   encounter_type = EncounterType.find(:all,:conditions =>["name IN(?)",art_encounters]).map(&:id)
-                                                                                
+                                                                       
   Patient.find(:all,:joins =>"INNER JOIN encounter e                            
     ON e.patient_id = patient.patient_id AND e.voided = 0",                     
     :conditions =>["encounter_type IN (?)",encounter_type], 
