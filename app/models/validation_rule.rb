4d085bd9c9bcbdbf59c893f805cdabb63e6422d1
timmtonga <timmtonga@gmail.com>
2014-02-21 13:28:10 +0200

    removed writing or validation results in method validating presence of reason for starting

--- app/models/validation_rule.rb
+++ app/models/validation_rule.rb
@@ -15,10 +15,7 @@ class ValidationRule < ActiveRecord::Base
     patient_ids = PatientProgram.find_by_sql("SELECT patient_id FROM earliest_start_date where patient_id NOT IN
                 (SELECT distinct person_id from obs where concept_id = #{start_reason_concept} and voided = 0)")
 
-    if !patient_ids.blank?
-      violated_rule_id = ValidationRule.find_by_desc("Patients without reason for starting ARVs").id
-      ValidationResult.create(:rule_id => violated_rule_id, :failures => patient_ids.length, :date_checked => Date.today)
-    end
+    return patient_ids
 
   end
 
