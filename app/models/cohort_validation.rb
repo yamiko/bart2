2c236b16837c2ab772cdf45fce867719abe14b15
Kenneth <vine@vine.(none)>
2014-02-24 11:05:58 +0200

    added a model for cohort validations

---
+++ app/models/cohort_validation.rb
@@ -0,0 +1,24 @@
+class cohort_validation
+    
+attr_accessor :cohort
+
+	def initialize(cohort)
+
+		self.cohort = cohort
+	
+	end
+
+	def get_all_differences
+
+		differences = {}
+		(self.class.instance_methods(false) - Object.methods).each do |method|
+			next if !method.match(/^validate\_/) 
+			validation = method.sub(/^validate\_/, "")
+			differences[validation] = self.send(method)
+		end
+
+		return differences
+	end
+			   
+end
+
