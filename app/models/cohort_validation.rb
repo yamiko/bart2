4a40de3a13e7f5d83b75d00d9a7132246f493866
Kenneth <vine@vine.(none)>
2014-02-24 12:15:25 +0200

    documented code

--- app/models/cohort_validation.rb
+++ app/models/cohort_validation.rb
@@ -9,15 +9,24 @@ class CohortValidation
 	end
 
 	def get_all_differences
+	 
+	 #This code runs all methods that start with 'validate_' 
+	 #And returns a hash of all return values from such methods
+	 #By Kenneth Kapundi
+	 
 
 		differences = {}
 		(self.class.instance_methods(false) - Object.methods).each do |method|
 			next if !method.match(/^validate\_/) 
-			validation = method.sub(/^validate\_/, "")
-			differences[validation] = self.send(method)
+			validation = method.sub(/^validate\_/, "").humanize
+			differences[validation] = self.send(method) unless validation.blank?
 		end
 
 		return differences
+	end
+	
+	def validate_karposis_sarcoma_greator_than_total
+		return true
 	end
 			   
 end
