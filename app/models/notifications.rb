7333b03a093809ea41e7a1c2a4ab6f5f7938c608
Fuvu Chirwa <fuvu.chirwa@gmail.com>
2014-03-10 17:27:57 +0200

    optimising email sending

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -1,31 +1,34 @@
 class Notifications < ActionMailer::Base
-include SendGrid
+
+  include SendGrid
   #default :from => "epics@baobabhealth.org"
   sendgrid_category :use_subject_lines
   sendgrid_enable   :ganalytics, :opentrack
   sendgrid_unique_args :key1 => Time.now, :key2 => Time.now
+  
+  def notify(subject,body,file_name)
+	 sendgrid_category "Report"
+	 sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
+     recipients 'fuvu.chirwa@gmail.com'
+     subject subject
+     from 'epics@baobabhealth.org'
+     content_type 'text/html'
+     part "text/html" do |p|
+       p.body = render_message("Test", :message =>"#{body}")
+     end
 
-  def notify(contact,subject,body,file_name)
-    #raise smtp_settings.to_yaml
-    sendgrid_category "Report"
-	  sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
-
-    subject   '#{subject}'
-    recipients '#{contact}'
-    from       'epics@baobabhealth.org'
-    sent_on    Date.today
-    #sent_at    Time.now
-    body       '#{body}'
-    attachment :content_type => "application/pdf",
-               :body => File.read("#{file_name}")
-
+    # attachment :content_type => "application/pdf",
+     #           :body => File.read("/tmp/#{file_name}")
+     @subject = subject
+     @email_body = body
+   #  mail(:to => 'fuvu.chirwa@gmail.com', :subject => @subject)
   end
 
   def email_error(subject)
     sendgrid_category "Email Error"
 	  sendgrid_unique_args :key2 => Time.now, :key3 => Time.now
     @subject = subject
-    mail(:to => "epics@baobabhealth.org", :subject => @subject)
+    mail(:to => "fuvu.chirwa@gmail.com", :subject => @subject)
   end
 
 end
