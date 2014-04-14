ac21caf957a97b7bcc28bdfda0b3b4f74ec35959
Ernest Matola <mangochiman@gmail.com>
2014-04-14 14:57:57 +0200

    Working on viral load

--- app/controllers/generic_people_controller.rb
+++ app/controllers/generic_people_controller.rb
@@ -376,6 +376,10 @@ class GenericPeopleController < ApplicationController
     @vl_request = Observation.find(:last, :conditions => ["person_id = ? AND concept_id = ?",
             patient.patient_id, Concept.find_by_name("Viral load").concept_id]
         ).answer_string.squish.upcase rescue nil
+
+    @date_vl_result_given = Observation.find(:last, :conditions => ["
+        person_id =? AND concept_id =? AND value_text REGEXP ?", @person.id,
+        Concept.find_by_name("Viral load").concept_id, 'Result given to patient']).value_datetime rescue nil
     
 		render :layout => false
 	end
