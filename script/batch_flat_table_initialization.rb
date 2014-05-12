cfaf485a23009184c30bccf162bc0507320ec594
Precious Bondwe <precious.bondwe@baobabhealth.org>
2014-05-12 12:10:12 +0200

    created a skeleton for writing sql statements to file

--- script/batch_flat_table_initialization.rb
+++ script/batch_flat_table_initialization.rb
@@ -4,13 +4,43 @@ def initialize
   started_at = Time.now.strftime("%Y-%m-%d-%H%M%S")
 end
 
-def write_sql(hash,table)
+def write_sql(a_hash,table)
 
- $temp_ft1_outfile = File.open("./migration_output/flat_table_1-" + started_at + ".sql", "w")
- $temp_ft2_outfile = File.open("./migration_output/flat_table_2-" + started_at + ".sql", "w") 
- $temp_cft_outfile = File.open("./migration_output/cohort_flat_table-" + started_at + ".sql", "w")
+    #open the necessary output file for writing
+    if table == 'f1'
+        $temp_outfile = File.open("./migration_output/flat_table_1-" + started_at + ".sql", "w")
+	initial_text = "INSERT INTO flat_table1 "
+    elsif table == 'f2'
+ 	$temp_outfile = File.open("./migration_output/flat_table_2-" + started_at + ".sql", "w") 
+	initial_text = "INSERT INTO flat_table2 "
+    elsif table == 'cft'
+	$temp_outfile = File.open("./migration_output/cohort_flat_table-" + started_at + ".sql", "w")
+	initial_text = "INSERT INTO cohort_flat_table "
+    else
+	raise "Invalid table"
+    end
+    #initialize field and values variables
+    fields = ""
+    values = ""
+   
+    #create sql statement
+    a_hash.each do |key,value|
+	fields = "'#{key}', "
+	values = "'#{value}', "	
+    end
+    full_string = initial_text + "(" + fields + ")" + " VALUES (" + values + ")" + ";"
+    #write to output file
+    $temp_outfile << full_string
+
+    #close the output file
+    $temp_outfile.close
+end
+
+def get_patients
+
 end
 
 def start
  initialize
+ get_patients
 end
