ddaf53e6ac0bc0c017c8f02927d8ff5dfe425d9c
mwatha <mwathabwanali@gmail.com>
2014-05-12 16:39:30 +0200

    addding new prescription screen - prototype: 70% done

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -1,2 +1,18 @@
 class PrescriptionsController < GenericPrescriptionsController
+  def new_prescription
+    @partial_name = 'drug_set'
+    @partial_name = params[:screen] unless params[:screen].blank?
+    @drugs = Drug.find(:all,:limit => 100)
+    @drug_sets = {}
+    Drug.find(:all,:limit => 5,:order => "name DESC").each do |d|
+      @drug_sets[d.name] = { :duration => '30',:frequency => "OD",:dose => 2, :unit => 2 }
+    end
+    render :layout => false
+  end
+
+  def search_for_drugs
+    drugs = Drug.find(:all, :conditions => ["name LIKE (?)","%#{params[:searh_str]}%"])
+    render :text => "<li>" + drugs.map{|d| d.name } .join("</li><li>") + "</li>"
+  end
+
 end
