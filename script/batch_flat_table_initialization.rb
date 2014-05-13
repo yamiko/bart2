a5b9b5f68b36c9119a3f72d54cd33777e61822f6
Precious Bondwe <precious.bondwe@baobabhealth.org>
2014-05-13 13:18:55 +0200

    added other methods and also modified to other methods to ease the work

--- script/batch_flat_table_initialization.rb
+++ script/batch_flat_table_initialization.rb
@@ -29,6 +29,7 @@ def write_sql(a_hash,table)
 	values += values.empty? ? "`#{value}`" : ", `#{value}`"	
     end
     full_string = initial_text + "(" + fields + ")" + " VALUES (" + values + ");"
+    
     #write to output file
     $temp_outfile << full_string
 
@@ -50,17 +51,93 @@ def get_patients_data(patient_id)
 
 #list of encounters for bart2
 #vitals => 6, appointment => 7, treatment => 25, hiv clinic consultation => 53, hiv_reception => 51
+   initial_string = "INSERT INTO flat_table2 "
 
    visits.each do |visit|
+	# arrays of [fields, values]
+	vitals = []
+	appointment = []
+	hcc = []
+	hiv_reception = []	
+
 	encounters = Encounter.find(:all,
 			:include => [:observations],
+			:order => "encounter_datetime ASC"
 			:conditions => ['voided = 0 AND patient_id = ? AND date(encounter_datetime) = ?', patient_id, visit])
+	
+	encounters.each do |enc|
+		if enc.encounter_type = 6 #vitals
+			vitals = generate_vitals_string(encounter)
+		elsif
 
+		elsif
+
+		elsif
+
+		end
+	end
+
+   #write sql statement
+    sql_statement = initial_string + "(" + vitals[0] + appointment[0] + hcc[0] + hiv_reception[0] ")" + \
+		 " VALUES (" + vitals[1] + appointment[1] + hcc[1] + hiv_reception[1] + ");"
+	
+    $temp_outfile = File.open("./migration_output/flat_table_2-" + @started_at + ".sql", "w") 
+    $temp_outfile << sql_statement
+    $temp_outfile.close
 	
    end
+end
+def generate_vitals_string(encounter)
+
+    #initialize field and values variables
+    fields = ""
+    values = ""
+
+    #create vitals field list hash template
+    vitals_hash = {:height => 0,
+		   :weight => 0,
+		   :temperature => 0,
+		   :bmi => 0,
+		   :systolic_blood_pressure => 0,
+		   :diastolic_blood_pressure => 0,
+		   :weight_for_height => 0,
+		   :weight_for_age => 0,
+		   :height_for_age => 0
+		}
+
+    encounter.observations.each do |obs|
+	if obs.concept_id == 5089 #weight
+		vitals_hash[:weight] = obs.value_numeric
+	elsif obs.concept_id == 5090 #height
+		vitals_hash[:height] = obs.value_numeric
+	elsif obs.concept_id == 5088 #temperature
+		vitals_hash[:temperature] = obs.value_numeric
+	elsif obs.concept_id == 2137 #bmi
+		vitals_hash[:bmi] = obs.value_numeric
+        elsif obs.concept_id == 5085 #systolic blood pressure
+		vitals_hash[:systolic_blood_pressure] = obs.value_numeric
+        elsif obs.concept_id == 5086 #diastolic blood pressure
+		vitals_hash[:diastolic_blood_pressure] = obs.value_numeric
+        elsif obs.concept_id == 1822 #weight for height
+		vitals_hash[:weight_for_height] = obs.value_numeric
+        elsif obs.concept_id == 6396 #weight for age
+		vitals_hash[:weight_for_age] = obs.value_numeric
+        elsif obs.concept_id == 6397 #height_for_age 
+		vitals_hash[:height_for_age] = obs.value_numeric
+	end
+    end
+
+    #create sql statement
+    vitals_hash.each do |key,value|
+        fields += fields.empty? ? "`#{key}`" : ", `#{key}`"
+        values += values.empty? ? "`#{value}`" : ", `#{value}`"
+    end
+    
+    return [fields, values]
 end
 
 def start
  initialize
  get_all_patients
 end
+
