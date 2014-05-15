ecd5164e5f87b1b6b1b7d63687a961b1e6a4ab1b
Precious Bondwe <precious.bondwe@baobabhealth.org>
2014-05-15 17:00:48 +0200

    working on hiv clinic consultation encounter processing

--- script/batch_flat_table_initialization.rb
+++ script/batch_flat_table_initialization.rb
@@ -57,7 +57,7 @@ def get_patients_data(patient_id)
 	# arrays of [fields, values]
 	vitals = []
 	appointment = []
-	hcc = []
+	hiv_clinic_consultation = []
 	hiv_reception = []	
 
 	encounters = Encounter.find(:all,
@@ -68,10 +68,10 @@ def get_patients_data(patient_id)
 	encounters.each do |enc|
 		if enc.encounter_type == 6 #vitals
 			vitals = process_vitals_encounter(encounter)
-		elsif enc.encounter_type == #HIV reception
-
-		elsif
-
+		elsif enc.encounter_type == 51#HIV Reception
+			hiv_reception = process_hiv_reception_encounter(encounter)
+		elsif enc.encounter_type == 53 #HIV Clinic Consultation
+			hiv_clinic_consultation = process_hiv_clinic_consultation_encounter(encounter)
 		elsif
 
 		end
@@ -87,50 +87,6 @@ def get_patients_data(patient_id)
 	
    end
 end
-def process_vitals_encounter(encounter, type = 0) #type 0 normal encounter, 1 generate_template only 
-
-    #initialize field and values variables
-    fields = ""
-    values = ""
-
-    #create vitals field list hash template
-    vitals_hash = {:height => 0,
-		   :weight => 0,
-		   :temperature => 0,
-		   :bmi => 0,
-		   :systolic_blood_pressure => 0,
-		   :diastolic_blood_pressure => 0,
-		   :weight_for_height => 0,
-		   :weight_for_age => 0,
-		   :height_for_age => 0
-		}
-	
-    return generate_sql_string(vitals_hash) if type == 1 
-
-    encounter.observations.each do |obs|
-	if obs.concept_id == 5089 #weight
-		vitals_hash[:weight] = obs.value_numeric
-	elsif obs.concept_id == 5090 #height
-		vitals_hash[:height] = obs.value_numeric
-	elsif obs.concept_id == 5088 #temperature
-		vitals_hash[:temperature] = obs.value_numeric
-	elsif obs.concept_id == 2137 #bmi
-		vitals_hash[:bmi] = obs.value_numeric
-        elsif obs.concept_id == 5085 #systolic blood pressure
-		vitals_hash[:systolic_blood_pressure] = obs.value_numeric
-        elsif obs.concept_id == 5086 #diastolic blood pressure
-		vitals_hash[:diastolic_blood_pressure] = obs.value_numeric
-        elsif obs.concept_id == 1822 #weight for height
-		vitals_hash[:weight_for_height] = obs.value_numeric
-        elsif obs.concept_id == 6396 #weight for age
-		vitals_hash[:weight_for_age] = obs.value_numeric
-        elsif obs.concept_id == 6397 #height_for_age 
-		vitals_hash[:height_for_age] = obs.value_numeric
-	end
-    end
-
-    return generate_sql_string(vitals_hash)
-end
 
 def process_vitals_encounter(encounter, type = 0) #type 0 normal encounter, 1 generate_template only 
 
@@ -139,15 +95,24 @@ def process_vitals_encounter(encounter, type = 0) #type 0 normal encounter, 1 ge
     values = ""
 
     #create vitals field list hash template
-    a_hash = {:height => 0,
-                   :weight => 0,
-                   :temperature => 0,
-                   :bmi => 0,
-                   :systolic_blood_pressure => 0,
-                   :diastolic_blood_pressure => 0,
-                   :weight_for_height => 0,
-                   :weight_for_age => 0,
-                   :height_for_age => 0
+    a_hash = {  :height => 0,
+		:height_enc_id => 0,
+                :weight => 0,
+		:weight_enc_id => 0,
+                :temperature => 0,
+		:temperature_enc_id => 0,
+                :bmi => 0,
+		:bmi_enc_id => 0,
+                :systolic_blood_pressure => 0,
+		:systolic_blood_pressure_enc_id => 0,
+                :diastolic_blood_pressure => 0,
+		:diastolic_blood_pressure_enc_id => 0
+                :weight_for_height => 0,
+		:weight_for_height_enc_id => 0,
+                :weight_for_age => 0,
+		:weight_for_age_enc_id => 0,
+                :height_for_age => 0,
+		:height_for_age_enc_id => 0
                 }
 
     return generate_sql_string(a_hash) if type == 1
@@ -155,22 +120,31 @@ def process_vitals_encounter(encounter, type = 0) #type 0 normal encounter, 1 ge
     encounter.observations.each do |obs|
         if obs.concept_id == 5089 #weight
                 a_hash[:weight] = obs.value_numeric
+		a_hash[:weight_enc_id] = encounter.encounter_id
         elsif obs.concept_id == 5090 #height
                 a_hash[:height] = obs.value_numeric
+		a_hash[:height_enc_id] = encounter.encounter_id
         elsif obs.concept_id == 5088 #temperature
                 a_hash[:temperature] = obs.value_numeric
+		a_hash[:temperature_enc_id] = encounter.encounter_id
         elsif obs.concept_id == 2137 #bmi
                 a_hash[:bmi] = obs.value_numeric
+		a_hash[:bmi_enc_id] = encounter.encounter_id
         elsif obs.concept_id == 5085 #systolic blood pressure
                 a_hash[:systolic_blood_pressure] = obs.value_numeric
+		a_hash[:systolic_blood_pressure_enc_id] = encounter.encounter_id
         elsif obs.concept_id == 5086 #diastolic blood pressure
                 a_hash[:diastolic_blood_pressure] = obs.value_numeric
+		a_hash[:diastolic_blood_pressure_enc_id] = encounter.encounter_id
         elsif obs.concept_id == 1822 #weight for height
                 a_hash[:weight_for_height] = obs.value_numeric
+		a_hash[:weight_for_height_enc_id] = encounter.encounter_id
         elsif obs.concept_id == 6396 #weight for age
                 a_hash[:weight_for_age] = obs.value_numeric
+		a_hash[:weight_for_age_enc_id] = encounter.encounter_id 
         elsif obs.concept_id == 6397 #height_for_age 
                 a_hash[:height_for_age] = obs.value_numeric
+		a_hash[:height_for_age_enc_id] = encounter.encounter_id
         end
     end
 
@@ -218,6 +192,164 @@ def process_hiv_reception_encounter(encounter, type = 0) #type 0 normal encounte
 
     return generate_sql_string(a_hash)
 end
+
+def process_hiv_clinic_consultation_encounter(encounter, type = 0) #type 0 normal encounter, 1 generate_template only 
+
+    #initialize field and values variables
+    fields = ""
+    values = ""
+
+    #create vitals field list hash template
+    a_hash =   {:pregnant_yes => 'NULL',
+		:pregnant_yes_enc_id => '',
+		:pregnant_no => '',
+		:pregnant_no_enc_id => '',
+		:breastfeeding_yes => ''
+                :breastfeeding_yes_enc_id => 'NULL',
+		:breastfeeding_no => '',
+		:breastfeeding_no_enc_id => '',
+                :currently_using_family_planning_method_yes => 'NULL',
+                :currently_using_family_planning_method_yes_enc_id => 'NULL',
+                :currently_using_family_planning_method_no => 'NULL',
+                :currently_using_family_planning_method_no_enc_id => 'NULL',
+		:family_planning_method_oral_contraceptive_pills => '',
+		:family_planning_method_oral_contraceptive_pills_enc_id => '',
+		:family_planning_method_depo_provera => '',
+		:family_planning_method_depo_provera_enc_id => '',
+		:family_planning_method_intrauterine_contraception => '',
+		:family_planning_method_intrauterine_contraception_enc_id => '',
+		:family_planning_method_contraceptive_implant => '',
+		:family_planning_method_contraceptive_implant_enc_id => '',
+		:family_planning_method_male_condoms => '',
+		:family_planning_method_male_condoms_enc_id => '',
+		:family_planning_method_female_condoms => '',
+		:family_planning_method_female_condoms_enc_id => '',
+		:family_planning_method__rythm_method => '',
+		:family_planning_method__rythm_method_enc_id => '',
+		:family_planning_method_withdrawal => '',
+		:family_planning_method_withdrawal_enc_id => '',
+		:family_planning_method_abstinence => '',
+		:family_planning_method_abstinence_enc_id => '',
+		:family_planning_method_tubal_ligation => '',
+		:family_planning_method_tubal_ligation_enc_id => '',
+		:family_planning_method_emergency__contraception => '',
+		:family_planning_method_emergency__contraception_enc_id => '',
+		:family_planning_method_vasectomy => '',
+		:family_planning_method_vasectomy_enc_id => '',
+                :family_planning_method_used => 'NULL',
+		:symptom_present_lipodystrophy => '',        
+		:symptom_present_lipodystrophy_enc_id => '',        
+		:symptom_present_anemia => '',        
+		:symptom_present_anemia_enc_id => '',        
+		:symptom_present_jaundice => '',        
+		:symptom_present_jaundice_enc_id => '',        
+		:symptom_present_lactic_acidosis => '',        
+		:symptom_present_lactic_acidosis_enc_id => '',        
+		:symptom_present_fever => '',        
+		:symptom_present_fever_enc_id => '',        
+		:symptom_present_skin_rash => '',        
+		:symptom_present_skin_rash_enc_id => '',        
+		:symptom_present_abdominal_pain => '',        
+		:symptom_present_abdominal_pain_enc_id => '',        
+		:symptom_present_anorexia => '',        
+		:symptom_present_anorexia_enc_id => '',        
+		:symptom_present_cough => '',        
+		:symptom_present_cough_enc_id => '',        
+		:symptom_present_diarrhea => '',        
+		:symptom_present_diarrhea_enc_id => '',        
+		:symptom_present_hepatitis => '',        
+		:symptom_present_hepatitis_enc_id => '',        
+		:symptom_present_leg_pain_numbness => '',        
+		:symptom_present_leg_pain_numbness_enc_id => '',        
+		:symptom_present_peripheral_neuropathy => '',        
+		:symptom_present_peripheral_neuropathy_enc_id => '',        
+		:symptom_present_vomiting => '',        
+		:symptom_present_vomiting_enc_id => '',        
+		:symptom_present_other_symptom => '',        
+		:symptom_present_other_symptom_enc_id => '',        
+		:side_effects_peripheral_neuropathy => '',
+		:side_effects_peripheral_neuropathy_enc_id => '',
+		:side_effects_peripheral_hepatitis => '',
+		:side_effects_peripheral_hepatitis_enc_id => '',
+		:side_effects_peripheral_skin_rash => '',
+		:side_effects_peripheral_skin_rash_enc_id => '',
+		:side_effects_peripheral_lipodystrophy => '',
+		:side_effects_peripheral_lipodystrophy_enc_id => '',
+		:side_effects_peripheral_other => '',
+		:side_effects_peripheral_other_enc_id => '',
+		:drug_induced_abdominal_pain => '',
+		:drug_induced_abdominal_pain_enc_id => '',
+		:drug_induced_anorexia => '',
+		:drug_induced_anorexia_enc_id => '',
+		:drug_induced_diarrhea => '',
+		:drug_induced_diarrhea_enc_id => '',
+		:drug_induced_jaundice => '',
+		:drug_induced_jaundice_enc_id => '',
+		:drug_induced_leg_pain_numbness => '',
+		:drug_induced_leg_pain_numbness_enc_id => '',
+		:drug_induced_vomiting => '',
+		:drug_induced_vomiting_enc_id => '',
+		:drug_induced_peripheral_neuropathy => '',
+		:drug_induced_peripheral_neuropathy_enc_id => '',
+		:drug_induced_hepatitis => '',
+		:drug_induced_hepatitis_enc_id => '',
+		:drug_induced_anemia => '',
+		:drug_induced_anemia_enc_id => '',
+		:drug_induced_lactic_acidosis => '',
+		:drug_induced_lactic_acidosis_enc_id => '',
+		:drug_induced_lipodystrophy => '',
+		:drug_induced_lipodystrophy_enc_id => '',
+		:drug_induced_skin_rash => '',
+		:drug_induced_skin_rash_enc_id => '',
+		:drug_induced_other_symptom => '',
+		:drug_induced_other_symptom_enc_id => '',
+		:drug_induced_fever => '',
+		:drug_induced_fever_enc_id => '',
+		:drug_induced_cough => '',
+		:drug_induced_cough_enc_id => '',
+        :abdominal_pains => 'NULL',
+                :anorexia => 'NULL',
+                :cough => 'NULL',
+                :diarrhoea => 'NULL',
+	 	:fever => '',
+	 	:jaundice => '',
+		:leg_pain_numbness => '',
+		:vomit => '',
+		:weight_loss => '',
+		:peripheral_neuropathy => '',
+		:hepatitis => '',
+		:anaemia => '',
+		:lactic_acidosis => '',
+		:lipodystrophy => '',
+		:skin_rash => '',
+		:other_symptoms => ''
+                }
+
+    return generate_sql_string(a_hash) if type == 1
+
+    encounter.observations.each do |obs|
+        if obs.concept_id == 2122 #Guardian Present
+                if obs.value_coded == 1065 && obs.value_coded_name_id == 1102
+                        a_hash[:guardian_present_yes] = 'Yes'
+                        a_hash[:guardian_present_yes_enc_id] = encounter.encounter_id
+                elsif obs.value_coded == 1066 && obs.value_coded_name_id == 1103
+                        a_hash[:guardian_present_no] = 'No'
+                        a_hash[:guardian_present_no_enc_id] = encounter.encounter_id
+                end
+        elsif obs.concept_id == 1805 #Patient Present
+                if obs.value_coded == 1065 && obs.value_coded_name_id == 1102
+                        a_hash[:patient_present_yes] = 'Yes'
+                        a_hash[:patient_present_yes_enc_id] = encounter.encounter_id
+                elsif obs.value_coded == 1066 && obs.value_coded_name_id == 1103
+                        a_hash[:patient_present_no] = 'No'
+                        a_hash[:patient_present_no_enc_id] = encounter.encounter_id
+                end
+        end
+    end
+
+    return generate_sql_string(a_hash)
+end
+
 
 def generate_sql_string(a_hash)
 
