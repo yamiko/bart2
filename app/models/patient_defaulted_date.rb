ea1ef3910a9d322e6273a817f83b280e5cedd4f1
Deliwe <dbnkhoma@gmail.com>
2014-07-30 10:18:50 +0200

    updated patient_defaulted_dates and also added patient_max_defaulted_date function

--- app/models/patient_defaulted_date.rb
+++ app/models/patient_defaulted_date.rb
@@ -7,54 +7,49 @@ ActiveRecord::Base.connection.execute <<EOF
     DELETE FROM patient_defaulted_dates;
 EOF
 
-    patients = Patient.find_by_sql("SELECT patient_id FROM earliest_start_date limit 20")
+ActiveRecord::Base.connection.execute <<EOF
+    DELETE FROM patient_max_defaulted_dates;
+EOF
+
+    start_time = Time.now.to_s
+    patients = Patient.find_by_sql("SELECT patient_id FROM earliest_start_date WHERE patient_id IN (2993, 4215, 4346, 4598,438, 1248, 1609, 2353, 2439, 3273, 3361, 3700, 4463, 5115, 5119, 5902, 6062, 6487, 6578, 6724, 6941, 7118, 7855, 7874, 7973, 8296, 8420)")
 
     patients.each do |patient|
       puts ">>>>>>>> working on patient_id: #{patient.patient_id} >>>>>>>>>>"
 ActiveRecord::Base.connection.execute <<EOF
-INSERT INTO patient_defaulted_dates (patient_id, order_id, drug_id, equivalent_daily_dose, amount_dispensed, pills_remaining, start_date, end_date, defaulted_date)
-select 
+INSERT INTO patient_defaulted_dates (patient_id, order_id, drug_id, equivalent_daily_dose, amount_dispensed, quantity_given, start_date, end_date, defaulted_date)
+SELECT 
     o.person_id,
     o.order_id,
     ad.drug_inventory_id,
     ad.equivalent_daily_dose,
-    IFNULL(o.value_numeric, o.value_text) as amount_dispensed,
-    IFNULL(p.value_numeric, p.value_text) as pill_count,
+    SUM(IFNULL(o.value_numeric, o.value_text)) as amount_dispensed,
+    ad.quantity,
     ord.start_date,
     ADDDATE(ord.start_date,
-        (IFNULL(IFNULL(o.value_numeric, o.value_text), 0) + IFNULL(IFNULL(p.value_numeric, p.value_text), 0)) / ad.equivalent_daily_dose) as end_date,
-ADDDATE(ADDDATE(ord.start_date,
-        (IFNULL(IFNULL(o.value_numeric, o.value_text), 0) + IFNULL(IFNULL(p.value_numeric, p.value_text), 0)) / ad.equivalent_daily_dose), 56) as defaulted_date
-from
+        (IFNULL(ad.quantity, 0) / IFNULL(ad.equivalent_daily_dose, 2))) as end_date,
+    ADDDATE(ADDDATE(ord.start_date,
+            IFNULL((IFNULL(ad.quantity, 0) / IFNULL(ad.equivalent_daily_dose, 2)),
+                    28)),
+        56) AS defaulted_date
+FROM
     obs o
-        inner join
+        INNER JOIN
     orders ord ON ord.order_id = o.order_id
-        inner join
+        INNER JOIN
     drug_order ad ON ord.order_id = ad.order_id
-        inner join
+        INNER JOIN
     arv_drug av ON av.drug_id = ad.drug_inventory_id
-        left join
-    (select 
-        ob.person_id,
-            ob.encounter_id,
-            ob.order_id,
-            ob.obs_datetime,
-            ob.value_numeric,
-            ob.value_text
-    from
-        obs ob
-    inner join orders ord ON ord.order_id = ob.order_id
-    inner join drug_order ad ON ord.order_id = ad.order_id
-    inner join arv_drug av ON av.drug_id = ad.drug_inventory_id
-    where ob.person_id = #{patient.patient_id}
-        AND ob.concept_id = 2540
-            and ob.voided = 0) as p ON p.person_id = o.person_id
-        and p.order_id = o.order_id
-where o.person_id = #{patient.patient_id}
+WHERE
+    o.person_id = #{patient.patient_id}
         AND o.concept_id = 2834
-        and o.voided = 0
-order by o.order_id;
+        AND o.voided = 0
+GROUP BY o.order_id, ad.drug_inventory_id;
 EOF
     end
+    end_time = Time.now.to_s
+    puts ">>>>>> total_patients: #{patients.length}"
+    puts ">>>>>> started_at: #{start_time}"
+    puts ">>>>>> finished_at: #{end_time}"
   end
 end
--- db/migrate/20140728112036_create_patient_defaulted_dates.rb
+++ db/migrate/20140728112036_create_patient_defaulted_dates.rb
@@ -1,12 +1,12 @@
 class CreatePatientDefaultedDates < ActiveRecord::Migration
   def self.up
     create_table :patient_defaulted_dates do |t|
-      t.integer :patient_id                                                        
+      t.integer :patient_id                              
       t.integer :order_id
-      t.integer :drug_id   
+      t.integer :drug_id
       t.float   :equivalent_daily_dose
       t.integer :amount_dispensed
-      t.integer :pills_remaining
+      t.integer :quantity_given
       t.date    :start_date
       t.date    :end_date
       t.date    :defaulted_date
